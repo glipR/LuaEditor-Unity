b6a60029becdb5022be73c53b70b63ddb1ca358a
Jackson Goerner <37640160+glipR@users.noreply.github.com>
2020-04-03 14:55:16 +1100

    Hook up to show code field and execute

---
+++ Assets/CodeDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CodeDisplay : MonoBehaviour {
+
+    public TextMeshProUGUI text;
+    public TMP_InputField inputField;
+    public string stringText;
+
+    private void Start() {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void setText(string t) {
+        stringText = t;
+        text.text = CodeStyler.Style(t);
+    }
+
+    public void executeText() {
+        ScriptRunner.instace.runText(stringText);
+    }
+
+}
--- Assets/ScriptRunner.cs
+++ Assets/ScriptRunner.cs
@@ -3,31 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using MoonSharp;
 using MoonSharp.Interpreter;
+using MoonSharp.Interpreter.Loaders;
 
 public class ScriptRunner : MonoBehaviour {
+
+    Script script;
+    public static ScriptRunner instace;
+
+    public void runText(string t) {
+        script.DoString(t);
+    }
+
     void Start() {
+        instace = this;
         UserData.RegisterProxyType<BoxApiProxy, BoxApiType>(r => new BoxApiProxy(r));
-        Script S = new Script(MoonSharp.Interpreter.CoreModules.Preset_SoftSandbox);
-        S.Options.DebugPrint = s => { Debug.Log(s); };
-        S.Globals["api"] = new BoxApiType();
-        S.DoString(@"
-            for i = 0,2,1 do
-                api.MoveBox(0, -1)
-                api.MoveBox(-1, 0)
-            end
-            for x = 0,4,1
-            do
-                for i = 0,4,1
-                do
-                    if x % 2 == 0 then
-                        api.MoveBox(0, 1)
-                    else
-                        api.MoveBox(0, -1)
-                    end
-                end
-                api.MoveBox(1, 0)
-            end
-        ");
+        // MAKE THIS SOFT SANDBOX
+        script = new Script(MoonSharp.Interpreter.CoreModules.Preset_Default);
+        ((ScriptLoaderBase)script.Options.ScriptLoader).ModulePaths = new string[] { "Assets/Resources/MoonSharp/Scripts/?" };
+        script.Options.DebugPrint = s => { Debug.Log(s); };
+        script.Globals["api"] = new BoxApiType();
     }
 
 }
